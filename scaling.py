051affce5622ec5048bc63dcbe7502351dcb3afc
Alif Amarta <alifamarta23@gmail.com>
2025-12-30 21:26:48 +0700

    Update: add scaling.py

---
+++ scaling.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+def scale_features(
+        df: pd.DataFrame,
+        columns: list,
+        method: str = "standard"
+) -> pd.DataFrame:
+    
+    df = df.copy()
+
+    if method == "standard":
+        scaler = StandardScaler()
+
+    elif method == "minmax":
+        scaler = MinMaxScaler()
+
+    else:
+        raise ValueError("Scaling method must be 'standard' or 'minmax'")
+    
+    df[columns] = scaler.fit_transform(df[columns])
+    return df
